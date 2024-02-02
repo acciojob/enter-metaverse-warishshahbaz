7f17cffd88a086b27b1c9dd99b349f09fb93d2c6
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-02 23:37:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 
 function handleClicked(){
-	let text = document.getElementById("text");
-	text.innerHTML = "Entered Metaverse"
+	let text = document.getElementById("status");
+	text.innerHTML =`<h1>Entered Metaverse</h1>`;
 }
